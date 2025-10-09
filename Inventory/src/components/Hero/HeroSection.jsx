7badd63a6e6596c37aea4fac69259ae9f73ad6bf
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-10-09 22:57:36 +0530

    Hero

--- Inventory/src/components/Hero/HeroSection.jsx
+++ Inventory/src/components/Hero/HeroSection.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <section>
-        <div className='flex mx-auto max-w-2xl col-1'>
-            <h1>Inventory management software designed for Indian businesses</h1>
-            <p>Manage orders. Track inventory. Handle GST billing. Oversee warehouses. 
+        <div className='flex mx-auto max-w-2xl flex-col px-8 py-8 md:py-16'>
+            <h1 className='text-xl text-gray-600 md:text-3xl text-center font-bold pb-4'>Inventory management software designed for Indian businesses</h1>
+            <p className=' text-center pb-8'>Manage orders. Track inventory. Handle GST billing. Oversee warehouses. 
                 One inventory management software to run all your inventory operations.</p>
+          <div className='flex space-x-4 justify-center'>
+          <button className='bg-red-500 hover:bg-red-600 rounded-lg  px-6 py-2 text-white font-bold '>SIGN UP -IT'S FREE</button>
+          <button className=' border border-red-500 hover:bg--600 text-red-600 hover:color bg-[var(--primary)]/60 cursor-pointer rounded-lg  px-6 py-2  font-bold '>EXPLORE DEMO ACCOUNT</button>
+          </div>
         </div>
     </section>
   )
