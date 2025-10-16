240fb71b0e0da0609f9060488353fddc6493c74c
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-10-16 21:17:05 +0530

    add

--- Inventory/Client/src/components/dashboard/sidepage/AddProduct.jsx
+++ Inventory/Client/src/components/dashboard/sidepage/AddProduct.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddProduct() {
+  const[select,setSelect]=useState(false)
   return (
     <section className='bg-white p-4 w-full'>
         <div>
             <h1> New Item </h1>
 
             <div>
-              <form >
+              <form className='flex gap-2' >
                 
                 <h2>Type</h2>
-                <div className='flex'>
-                <input type="radio" id='goods'/>
+                <div className='flex gap-2'>
+                <input type="radio" id='goods' name='type' value='goods' checked={select === 'goods'} className='cursor-pointer'  onChange={(e)=>setSelect(e.target.value) }/>
                 <label htmlFor="goods">Goods</label>
-                <input type="radio" id='services'/>
-                <label htmlFor="goods">Services</label>
+                <input type="radio" id='services' value='services' checked={select === 'services'} className='cursor-pointer'  onChange={(e)=>setSelect(e.target.value)}/>
+                <label htmlFor="services">Services</label>
                 </div>
+
+                <label htmlFor="">Name</label>
+                <input type="text" value="name" name='name'/>
               </form>
             </div>
         </div>
