f5d81c3f7106bbd5de491edc19849a27d42535aa
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-11-07 13:11:33 +0530

    inventory

--- Inventory/Client/src/components/dashboard/sidepage/AddProduct.jsx
+++ Inventory/Client/src/components/dashboard/sidepage/AddProduct.jsx
@@ -2,26 +2,28 @@ import React, { useState } from 'react'
 
 export default function AddProduct() {
   const[select,setSelect]=useState(false)
+const title=["DOC NO","DATE","JOB ORDER NO","RECORD TYPE","FABRIC GROUP","COLOR NAME","SET NO","S.NO","DC DIA","DIA TYPE",
+  "PRCESS NAME","PROCESS DC NO","COMPACT NO","RECD DC NO","RECD DC DATE","RECD DC ROLL","RECD DC WGT"]
+
   return (
     <section className='bg-white p-4 w-full'>
         <div>
             <h1> New Item </h1>
-
-            <div>
-              <form className='flex gap-2' >
-                
-                <h2>Type</h2>
-                <div className='flex gap-2'>
-                <input type="radio" id='goods' name='type' value='goods' checked={select === 'goods'} className='cursor-pointer'  onChange={(e)=>setSelect(e.target.value) }/>
-                <label htmlFor="goods">Goods</label>
-                <input type="radio" id='services' value='services' checked={select === 'services'} className='cursor-pointer'  onChange={(e)=>setSelect(e.target.value)}/>
-                <label htmlFor="services">Services</label>
-                </div>
-
-                <label htmlFor="">Name</label>
-                <input type="text" value="name" name='name'/>
-              </form>
-            </div>
+           <form className='w-full'>
+                  {
+                    title.map((item,index)=>(
+                      <div key={index} className='flex gap-1 space-y-2 justify-center '>
+                        <h2 className='text-xl'>{index+1}</h2>
+                        <p>.</p>
+                        <label className='text-xl w-1/4' >{item}</label>
+                        <input className='w-3/4'></input>
+                      </div>
+                    ))
+                  }
+                  <button className='text-center border-2 bg-blue-200 text-white text-xl hover:bg-blue-300
+                    cursor-pointer p-2 rounded-xl w-full'>SUBMIT</button>
+                  </form>
+   
         </div>
     </section>
   )
